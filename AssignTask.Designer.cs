3bf36bb270d4e87c1ec8ed7320ff643df4557d2f
Muaz Elahi <119494535+perogrammer@users.noreply.github.com>
2024-04-28 19:24:51 +0500

    muaz elahi
    asdf

---
+++ AssignTask.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Societies_Management_System
+{
+    partial class AssignTask
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logo0jToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // logo0jToolStripMenuItem
+            // 
+            this.logo0jToolStripMenuItem.Name = "logo0jToolStripMenuItem";
+            this.logo0jToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.logo0jToolStripMenuItem.Text = "Back";
+            this.logo0jToolStripMenuItem.Click += new System.EventHandler(this.logo0jToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.logo0jToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 19;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Title";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Assign To:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Description:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Assign Task";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(21, 74);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(283, 114);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 61);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(327, 342);
+            this.panel1.TabIndex = 20;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(21, 218);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 21);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 353;
+            this.splitContainer1.TabIndex = 21;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 20, 0, 0);
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(443, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // AssignTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.splitContainer1);
+            this.MaximizeBox = false;
+            this.Name = "AssignTask";
+            this.Text = "Task Management";
+            this.Load += new System.EventHandler(this.AssignTask_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripMenuItem logo0jToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
---
+++ AssignTask.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+
+namespace Societies_Management_System
+{
+    public partial class AssignTask : Form
+    {
+        private int societyId = SocietyDetails.societyId;
+        public AssignTask()
+        {
+            InitializeComponent();
+        }
+        private void AssignTask_Load(object sender, EventArgs e)
+        {
+            PopulateMembersDropdown();
+            DisplayTaskView();
+        }
+        private void PopulateMembersDropdown()
+        {
+            try
+            {
+                DatabaseHandler dbHandler = new DatabaseHandler();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "SELECT username FROM users " +
+                                      "JOIN society_members ON users.user_id=society_members.user_id " +
+                                      "WHERE society_id=@societyId";
+                command.Parameters.AddWithValue("@societyId", societyId);
+
+                DataTable result = dbHandler.ExecuteQuery(command);
+                foreach (DataRow row in result.Rows)
+                {
+                    string username = row["username"].ToString();
+                    comboBox1.Items.Add(username);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private int GetUserIdFromUsername(string username)
+        {
+            try
+            {
+                DatabaseHandler dbHandler = new DatabaseHandler();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "SELECT user_id FROM users WHERE username = @username";
+                command.Parameters.AddWithValue("@username", username);
+                object result = dbHandler.ExecuteQueryScalar(command);
+                if (result != null)
+                {
+                    return Convert.ToInt32(result);
+                }
+                else
+                {
+                    MessageBox.Show("User not found.");
+                    return -1;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                return -1;
+            }
+        }
+
+        private bool InsertTask(string name, string description, int assignedToUserId)
+        {
+            try
+            {
+                DatabaseHandler dbHandler = new DatabaseHandler();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "INSERT INTO tasks (society_id, name, description, assigned_to) VALUES (@societyId, @name, @description, @assignedToUserId)";
+                command.Parameters.AddWithValue("@societyId", societyId);
+                command.Parameters.AddWithValue("@name", name);
+                command.Parameters.AddWithValue("@description", description);
+                command.Parameters.AddWithValue("@assignedToUserId", assignedToUserId);
+                dbHandler.ExecuteNonQuery(command);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                return false;
+            }
+        }
+
+
+        private void ClearForm()
+        {
+            textBox1.Text = "";
+            richTextBox1.Text = "";
+            comboBox1.SelectedIndex = -1;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string taskName = textBox1.Text;
+            string taskDescription = richTextBox1.Text;
+            string assignedToUsername = comboBox1.SelectedItem?.ToString();
+
+            if (!string.IsNullOrEmpty(assignedToUsername))
+            {
+                int assignedToUserId = GetUserIdFromUsername(assignedToUsername);
+                if (assignedToUserId != -1)
+                {
+                    if (!string.IsNullOrEmpty(taskName) && !string.IsNullOrEmpty(taskDescription))
+                    {
+                        if (InsertTask(taskName, taskDescription, assignedToUserId))
+                        {
+                            MessageBox.Show("Task assigned successfully.");
+                            ClearForm();
+                            DisplayTaskView();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Failed to assign task.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter both task name and description.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Failed to assign task. User not found.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a member to assign the task to.");
+            }
+        }
+        private void DisplayTaskView()
+        {
+            DataTable tasksTable = GetTasksDataFromDatabase(); 
+            int top = 10;
+            tableLayoutPanel1.Controls.Clear();
+            foreach (DataRow row in tasksTable.Rows)
+            {
+                string taskName = row["name"].ToString();
+                string taskDescription = row["description"].ToString();
+                string taskStatus = row["status"].ToString();
+                if (taskStatus == "completed")
+                    continue;
+
+                Panel taskPanel = new Panel();
+                taskPanel.BorderStyle = BorderStyle.None;
+                taskPanel.Width = tableLayoutPanel1.Width - 10;
+                taskPanel.Padding = new Padding(10);
+
+                Label nameLabel = new Label();
+                nameLabel.Text = $"Task Name: {taskName}";
+                nameLabel.AutoSize = true;
+                nameLabel.Location = new System.Drawing.Point(10, 10);
+                taskPanel.Controls.Add(nameLabel);
+
+                Label descriptionLabel = new Label();
+                descriptionLabel.Text = $"Description: {taskDescription}";
+                descriptionLabel.AutoSize = true;
+                descriptionLabel.Location = new System.Drawing.Point(10, 30);
+                taskPanel.Controls.Add(descriptionLabel);
+
+                Label statusLabel = new Label();
+                statusLabel.Text = $"Status: {taskStatus}";
+                statusLabel.AutoSize = true;
+                statusLabel.Location = new System.Drawing.Point(10, 50);
+                taskPanel.Controls.Add(statusLabel);
+
+                Button markCompletedButton = new Button();
+                markCompletedButton.Text = "Mark As Completed";
+                markCompletedButton.Tag = Convert.ToInt32(row["task_id"]);
+                markCompletedButton.AutoSize = true;
+                markCompletedButton.Location = new System.Drawing.Point(10, 70);
+                markCompletedButton.Click += MarkCompletedButton_Click;
+                taskPanel.Controls.Add(markCompletedButton);
+
+
+                taskPanel.Location = new System.Drawing.Point(20, top);
+                tableLayoutPanel1.Controls.Add(taskPanel);
+
+                top += taskPanel.Height + 10;
+            }
+        }
+
+        private void MarkCompletedButton_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            int taskId = (int)button.Tag;
+
+            try
+            {
+                DatabaseHandler dbHandler = new DatabaseHandler();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "UPDATE tasks SET status = 'completed' WHERE task_id = @taskId";
+                command.Parameters.AddWithValue("@taskId", taskId);
+                int rowsAffected = dbHandler.ExecuteNonQuery(command);
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Task marked as completed successfully.");
+                    DisplayTaskView();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to mark task as completed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private DataTable GetTasksDataFromDatabase()
+        {
+            try
+            {
+                DatabaseHandler dbHandler = new DatabaseHandler();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "SELECT * FROM tasks";
+                return dbHandler.ExecuteQuery(command);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                return null;
+            }
+        }
+
+        private void logo0jToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SocietyDetails societyDetails = new SocietyDetails(societyId);
+            societyDetails.Show();
+            this.Hide();
+        }
+    }
+}
